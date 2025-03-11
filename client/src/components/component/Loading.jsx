ee340ca2ae13e4cf4cafd30e2246b9d59eeeb383
surya <swainsuryakanta97@gmail.com>
2025-03-11 15:41:48 +0530

    loader update

--- client/src/components/component/Loading.jsx
+++ client/src/components/component/Loading.jsx
@@ -1,12 +1,12 @@
 import { useUser } from '@/context/userContext';
-import { LoaderCircleIcon } from 'lucide-react';
+import {RiseLoader} from "react-spinners"
 
 const Loading = ({children}) => {
     const {loading} = useUser()
   return (
     loading ? (
         <div className='min-h-screen min-w-full flex items-center justify-center bg-slate-900'>
-            <LoaderCircleIcon className='animate-spin text-emerald-600' size={60} />
+            <RiseLoader color='white' size={20}/>
         </div>
     ) : (
         <>
--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -50,16 +50,16 @@ const CheckoutPage = () => {
     },[user])
 
     return (
-        <div className="mt-5 md:mt-10 px-4 max-w-5xl mx-auto">
+        <div className="md:py-10 px-4 max-w-5xl mx-auto">
             <div className="flex flex-col md:flex-row justify-between items-center mb-6">
-                <h1 className="text-2xl md:text-5xl font-bold text-gray-800">Checkout</h1>
-                {isLoggedIn && <User size={32} className="text-gray-800 mt-2 md:mt-0" />}
+                <h1 className="text-2xl md:text-5xl font-bold ">Checkout</h1>
+                {isLoggedIn && <User size={32} className=" mt-2 md:mt-0" />}
             </div>
             
-            <div className="w-full bg-gray-100 p-4 md:p-6 rounded-lg shadow-lg overflow-x-hidden">
-                <table className="w-full bg-white rounded-lg shadow-md text-sm md:text-base">
+            <div className="w-full p-4 md:p-6 rounded-lg shadow-lg overflow-x-hidden">
+                <table className="w-full rounded-lg shadow-md text-sm md:text-base">
                     <thead>
-                        <tr className="bg-gray-200 text-gray-700 text-xs md:text-base">
+                        <tr className="text-xs md:text-base">
                             <th className="p-2 md:p-3">Image</th>
                             <th className="p-2 md:p-3 hidden md:table-cell">Product</th>
                             <th className="p-2 md:p-3">Price</th>
@@ -72,18 +72,18 @@ const CheckoutPage = () => {
                         {cartItems.map((item) => (
                             <tr key={item?._id} className="border-b text-xs md:text-base">
                                 <td className="p-2 md:p-3"><img src={item?.productImage} alt={item?.productName} className="w-12 h-12 md:w-16 md:h-16 object-contain" /></td>
-                                <td className="p-2 md:p-3 text-gray-900 font-semibold hidden md:table-cell">{item?.productName}</td>
+                                <td className="p-2 md:p-3  font-semibold hidden md:table-cell">{item?.productName}</td>
                                 <td className="p-2 md:p-3 text-green-600">${item?.productPrice}</td>
                                 <td className="p-2 md:p-3">
                                     <div className="flex items-center gap-1 md:gap-2">
-                                        <Button onClick={() => updateQuantity(item.id, item.quantity - 1)} className="bg-gray-300 text-black px-1 py-1 md:px-2 md:py-1">-</Button>
+                                        <Button onClick={() => updateQuantity(item.id, item.quantity - 1)} className=" px-1 py-1 md:px-2 md:py-1">-</Button>
                                         <span className="text-sm md:text-lg font-semibold">{item?.qty}</span>
-                                        <Button onClick={() => updateQuantity(item.id, item.quantity + 1)} className="bg-gray-300 text-black px-1 py-1 md:px-2 md:py-1">+</Button>
+                                        <Button onClick={() => updateQuantity(item.id, item.quantity + 1)} className=" px-1 py-1 md:px-2 md:py-1">+</Button>
                                     </div>
                                 </td>
-                                <td className="p-2 md:p-3 text-gray-900">${item?.productPrice * item?.qty}</td>
+                                <td className="p-2 md:p-3">${item?.productPrice * item?.qty}</td>
                                 <td className="p-2 md:p-3">
-                                    <Button onClick={() => removeItem(item.id)} className="bg-red-700 text-white px-2 py-1 md:px-3 md:py-1 hover:bg-red-600">Remove</Button>
+                                    <Button onClick={() => removeItem(item.id)} className="bg-red-700 px-2 py-1 md:px-3 md:py-1 hover:bg-red-600 dark:text-white">Remove</Button>
                                 </td>
                             </tr>
                         ))}
@@ -92,15 +92,15 @@ const CheckoutPage = () => {
                 <div className="text-lg md:text-xl font-semibold text-right mt-4">Total: ${totalP}</div>
             </div>
             
-            <div className="mt-6 p-4 md:p-6 bg-white rounded-lg shadow-lg">
-                <h2 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">Shipping Address</h2>
+            <div className="mt-6 p-4 md:p-6 rounded-lg shadow-lg">
+                <h2 className="text-xl md:text-2xl font-semibold mb-4">Shipping Address</h2>
                 {savedAddresses.length > 0 && (
                     <div className="mb-4">
-                        <h3 className="text-lg font-semibold text-gray-700">Saved Addresses</h3>
-                        <select className="w-full p-2 md:p-3 border border-gray-300 rounded-lg text-sm md:text-lg mt-2">
-                            <option value="">Select a saved address</option>
+                        <h3 className="text-lg font-semibold">Saved Addresses</h3>
+                        <select className="w-full p-2 md:p-3 border border-gray-300 rounded-lg text-sm md:text-lg mt-2 bg-transparent">
+                            <option value="" className='bg-transparent text-black'>Select a saved address</option>
                             {savedAddresses?.map((addr, index) => (
-                                <option key={index} value={addr?.fullName}>{addr?.fullName}, {addr?.phone}, {addr?.address1}, {addr?.address2}, {addr?.city}, {addr?.state}, {addr?.country}, {addr?.pincode}</option>
+                                <option className='bg-transparent text-black' key={index} value={addr?.fullName}>{addr?.fullName}, {addr?.phone}, {addr?.address1}, {addr?.address2}, {addr?.city}, {addr?.state}, {addr?.country}, {addr?.pincode}</option>
                             ))}
                         </select>
                     </div>
@@ -112,7 +112,7 @@ const CheckoutPage = () => {
                             (
                                 <input 
                                 key={key} 
-                                className="p-2 md:p-3 border border-gray-300 rounded-lg text-sm md:text-base" 
+                                className="p-2 md:p-3 border border-gray-300 rounded-lg text-sm md:text-base bg-transparent" 
                                 placeholder={key.replace(/([A-Z])/g, ' $1').trim()} 
                                 value={address[key]} 
                                 onChange={(e) => setAddress({...address, [key]: e.target.value})} 
